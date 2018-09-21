221bb98d5e0b7a3aeea19abe2174902056dde82f
h-hub <harsha.kj89@gmail.com>
2018-09-22 02:08:48 +0800

    enabling dynamic content

--- app.js
+++ app.js
@@ -2,12 +2,39 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+var addDynamicContent = function (content, request) {
+
+    var values = params(request);
+    var fileContent = content.toString();
+
+    for (var key in values) {
+        var regex = new RegExp("{{" + key + "}}");
+        fileContent = fileContent.replace(regex, values[key]);
+    }
+
+    return fileContent;
+}
+
+var params = function (req) {
+    let q = req.url.split('?'), result = {};
+    if (q.length >= 2) {
+        q[1].split('&').forEach((item) => {
+            try {
+                result[item.split('=')[0]] = item.split('=')[1];
+            } catch (e) {
+                result[item.split('=')[0]] = '';
+            }
+        })
+    }
+    return result;
+}
+
 http.createServer(function (request, response) {
     console.log('request starting...');
 
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.html';
+    var filePath = './public' + request.url.split('?')[0];
+    if (filePath == './public/')
+        filePath = './public/index.html';
 
     var extname = path.extname(filePath);
     var contentType = 'text/html';
@@ -23,7 +50,7 @@ http.createServer(function (request, response) {
             break;
         case '.png':
             contentType = 'image/png';
-            break;      
+            break;
         case '.jpg':
             contentType = 'image/jpg';
             break;
@@ -32,21 +59,22 @@ http.createServer(function (request, response) {
             break;
     }
 
-    fs.readFile(filePath, function(error, content) {
+    fs.readFile(filePath, function (error, content) {
         if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./public/404.html', function (error, content) {
                     response.writeHead(200, { 'Content-Type': contentType });
                     response.end(content, 'utf-8');
                 });
             }
             else {
                 response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end(); 
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                response.end();
             }
         }
         else {
+            content = addDynamicContent(content, request);
             response.writeHead(200, { 'Content-Type': contentType });
             response.end(content, 'utf-8');
         }
